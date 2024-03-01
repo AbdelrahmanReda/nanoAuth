649c185e4fb7f0fd23791d0b0a158a647b170699
abdelrahman-reda <boodycat09@gmail.com>
2024-03-02 00:21:21 +0200

    add secure

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(session({
     cookie: {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: false,
-        secure: false,
+        secure: true,
         sameSite: 'none',
         domain: "next-auth-app-six-delta.vercel.app"
     }
